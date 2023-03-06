c3b464ac83af422845db281e248d7b49783ff23b
Serhii Naboka <drifterzzz@gmail.com>
2023-03-06 16:15:32 +0200

    1st done
    1st done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryDivRef = document.querySelector('.gallery');
+const createGalleryMarkup = items => {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>
+`;
+    })
+    .join('');
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryDivRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
+console.log(galleryDivRef);
