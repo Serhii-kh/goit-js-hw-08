832c7811346e4421adcd92a79873a004f375b536
Serhii Naboka <drifterzzz@gmail.com>
2023-03-11 13:47:05 +0200

    2nd done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const setCurrentTime = e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+player.on('timeupdate', throttle(setCurrentTime, 1000));
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(currentTime);
